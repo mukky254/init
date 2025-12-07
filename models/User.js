081b1e38c2d1921e2849c709dfa6471cdc82c77a
mukky254 <muhidinyusuf06@gmail.com>
2025-12-07 16:06:23 +0300

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['student', 'lecturer', 'admin'],
+    required: true
+  },
+  admissionNumber: {
+    type: String,
+    sparse: true,
+    unique: true
+  },
+  phoneNumber: {
+    type: String
+  },
+  course: {
+    type: String
+  },
+  department: {
+    type: String
+  },
+  yearOfStudy: {
+    type: String
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  lastLogin: {
+    type: Date
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
